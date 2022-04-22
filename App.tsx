deeee1fab1f4aa9deca9468bfea8524bcd6433db
Jeremias Lima <jeremias.lima@etec.sp.gov.br>
2022-04-22 13:48:36 -0300

    Theme

--- App.tsx
+++ App.tsx
@@ -1,21 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import AppLoading from "expo-app-loading";
+
+import {
+  useFonts,
+  WorkSans_600SemiBold,
+  WorkSans_700Bold,
+  WorkSans_500Medium,
+} from "@expo-google-fonts/work-sans";
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    WorkSans_600SemiBold,
+    WorkSans_700Bold,
+    WorkSans_500Medium,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text style={{ fontFamily: "WorkSans_600SemiBold", fontSize: 40 }}>
+        Inter Black
+      </Text>
+
+      <Text style={{ fontFamily: "WorkSans_500Medium", fontSize: 40 }}>
+        Inter Black
+      </Text>
+
+      <Text style={{ fontFamily: "WorkSans_700Bold", fontSize: 40 }}>
+        Inter Black
+      </Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
